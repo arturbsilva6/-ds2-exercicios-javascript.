bfc4bf12a830b97e175a466ce96d26561010ff86
arturbsilva6 <arturbsilva6@gmail.com>
2026-08-25 19:08:53 -0300

    adiciona primeiro script e evidência do console

---
+++ js/exercicio01.js
@@ -0,0 +1,11 @@
+// Capturando as informações do usuário
+let nome = prompt('Digite seu nome completo:');
+const nomeCurso = prompt('Digite o curso:');
+let funcionalidade = prompt('Digite uma funcionalidade que gostaria de desenvolver com JavaScript:');
+
+console.log('Nome do aluno: ' + nome);
+console.log('Curso: ' + nomeCurso);
+console.log('Funcionalidade desejada: ' + funcionalidade);
+
+// Exibindo a mensagem final 
+console.log('JavaScript carregado com sucesso!');
